1ba21f5f0a947226fc1a6ce92a026cd7349e5cc5
Kenneth <kennethyoung.2960@gmail.com>
2024-03-13 21:22:30 -0500

    Caesar Cipher function working correctly with tests written and passed

--- Project_Decoder_ring_Node_18_1/src/caesar.js
+++ Project_Decoder_ring_Node_18_1/src/caesar.js
@@ -4,24 +4,46 @@
 // of the anonymous function on line 6
 
 const caesarModule = (function () {
-  // you can add any code you want within this function scope
+// you can add any code you want within this function scope
 
-  function caesar(input, shift, encode = true) {
-    let result = '';
-    for (let i = 0; i < message.length; i++) {
-      let char = message.charAt(i);
-    
-      if (char.match(/[a-zA-Z]/)) {
-        let code = char.charCodeAt(0);
 
-        
+function caesar(input, shift, encode = true) {
+  if (!shift || shift < -25 || shift > 25) {
+    return false;
+  }
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  input = input.toLowerCase();
+  let result = '';
+  
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i];
+
+    if (/[a-z]/.test(char)) {
+      let charIndex = alphabet.indexOf(char);
+      if (encode) {
+        charIndex = (charIndex + shift) % 26;
+        if (charIndex < 0) {
+          charIndex += 26; // Handle negative shift
+        }
+      } else {
+        charIndex = (charIndex - shift) % 26;
+        if (charIndex < 0) {
+          charIndex += 26; // Handle negative shift
+        }
       }
+
+      result += alphabet[charIndex];
+    } else {
+
+      result += char;
     }
   }
-
+  return result;
+}
   return {
     caesar,
   };
+
 })();
 
 module.exports = { caesar: caesarModule.caesar };
--- Project_Decoder_ring_Node_18_1/test/caesar.test.js
+++ Project_Decoder_ring_Node_18_1/test/caesar.test.js
@@ -1,1 +1,28 @@
 // Write your tests here!
+const assert = require('assert');
+const { caesar } = require('../src/caesar.js')
+
+describe('Caesar Cipher Decoder Tests', function() {
+  it('should decode a message with positive shift', function() {
+    assert.strictEqual(caesar('khoor, zruog!', 3, false), 'hello, world!');
+  });
+
+  it('should decode a message with negative shift', function() {
+    assert.strictEqual(caesar('dtz rfd gj rd lzjxy', 5, false), 'you may be my guest');
+  });
+
+  it('should maintain non-alphabetic characters', function() {
+    assert.strictEqual(caesar('khoor, zruog!', 3, false), 'hello, world!');
+  });
+
+  it('should return false if shift value is not within the range', function() {
+    assert.strictEqual(caesar('xyz', 27, false), false);
+  });
+});
+
+ // Add more tests as needed
+
+
+
+
+
