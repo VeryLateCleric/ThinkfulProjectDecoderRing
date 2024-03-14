26cc48eacf184fe3480258e394fb396de7d9ce8e
Kenneth <kennethyoung.2960@gmail.com>
2024-03-14 02:40:14 -0500

    Start of Substitution Codex

--- Project_Decoder_ring_Node_18_1/src/polybius.js
+++ Project_Decoder_ring_Node_18_1/src/polybius.js
@@ -7,16 +7,14 @@ const polybiusModule = (function () {
   // you can add any code you want within this function scope
 
   function polybius(input, encode = true) {
-  
-    const polybiusSquare = {
-      a: '11', b: '21', c: '31', d: '41', e: '51',
-      f: '12', g: '22', h: '32', i: '42', j: '42',
-      k: '52', l: '13', m: '23', n: '33', o: '43',
-      p: '53', q: '14', r: '24', s: '34', t: '44',
-      u: '54', v: '15', w: '25', x: '35', y: '45',
-      z: '55',
+  const polybiusSquare = {
+    a: '11', b: '21', c: '31', d: '41', e: '51',
+    f: '12', g: '22', h: '32', i: '42', j: '42',
+    k: '52', l: '13', m: '23', n: '33', o: '43',
+    p: '53', q: '14', r: '24', s: '34', t: '44',
+    u: '54', v: '15', w: '25', x: '35', y: '45',
+    z: '55',
   };
-
   const polybiusSquareDecode = {
     11: 'a', 21: 'b', 31: 'c', 41: 'd', 51: 'e',
     12: 'f', 22: 'g', 32: 'h', 42: '(i/j)', 52: 'k',
@@ -30,6 +28,7 @@ const polybiusModule = (function () {
     polybiusDecode[polybiusSquare[key]] = key;
   }
   const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
   const encodeHelper = (text) => {
     return text
       .toLowerCase()
@@ -58,13 +57,11 @@ const polybiusModule = (function () {
       .join(' ');
   };
 
-
   if (encode) {
     return encodeHelper(input)
   } else {
     return decodeHelper(input)
   }
-    // your solution code here
   }
 
   return {
