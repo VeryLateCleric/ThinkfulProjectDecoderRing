d8e228fe77ae41180915dc2dd159ef0a11cddce0
Kenneth <kennethyoung.2960@gmail.com>
2024-03-13 23:05:36 -0500

    Polybius decode started needs tests written

--- Project_Decoder_ring_Node_18_1/src/polybius.js
+++ Project_Decoder_ring_Node_18_1/src/polybius.js
@@ -7,6 +7,20 @@ const polybiusModule = (function () {
   // you can add any code you want within this function scope
 
   function polybius(input, encode = true) {
+  
+    const polybiusSquare = {
+      a: '11', b: '21', c: '31', d: '41', e: '51',
+      f: '12', g: '22', h: '32', i: '42', j: '42',
+      k: '52', l: '13', m: '23', n: '33', o: '43',
+      p: '53', q: '14', r: '24', s: '34', t: '44',
+      u: '54', v: '15', w: '25', x: '35', y: '45',
+      z: '55',
+  };
+
+  const polybiusDecode = {};
+  for (let key in polybiusSquare) {
+    polybiusDecode[polybiusSquare[key]] = key;
+  }
     // your solution code here
   }
 
